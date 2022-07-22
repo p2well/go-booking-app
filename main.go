afe2d85c6b452a37089daeba549abfa9675816e8
p2well <p2well@outlook.com>
2022-07-22 15:27:36 +0200

    Create an initial version

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+func main() {
+	conferenceName := "p2well's Go Conference"
+	const conferenceTickets int = 50
+	var remainingTickets uint =  50
+
+	// fmt.Printf("conferenceTickets is %T, remainingTickets is %T, conferenceName is %T\n", conferenceName, remainingTickets, conferenceName)
+	
+	fmt.Printf("Welcome to %v booking application\n", conferenceName)
+	fmt.Printf("We have total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
+	fmt.Println("Get your tickets here to attend")
+
+	var firstName string
+	var lastName string
+	var eMail string
+	var userTickets uint
+	// ask user for their name
+
+	// fmt.Println(remainingTickets)
+	// fmt.Println(&remainingTickets)
+
+	fmt.Print("Enter your first name: ")
+	fmt.Scan(&firstName)
+
+	fmt.Print("Enter your last name: ")
+	fmt.Scan(&lastName)
+
+	fmt.Print("Enter your e-mail address: ")
+	fmt.Scan(&eMail)
+
+	fmt.Print("Enter number of tickets: ")
+	fmt.Scan(&userTickets)
+
+	remainingTickets = remainingTickets - userTickets
+
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation e-mail at %v\n", firstName, lastName, userTickets, eMail)
+	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+	
+}
