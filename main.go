0c0cfbdffe67f1412f901c5d95177d9ad84c7eb8
p2well <p2well@outlook.com>
2022-07-29 13:49:17 +0200

    Add bookings

--- main.go
+++ main.go
@@ -6,7 +6,8 @@ func main() {
 	conferenceName := "p2well's Go Conference"
 	const conferenceTickets int = 50
 	var remainingTickets uint =  50
-		
+	var bookings [50]string
+	
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Printf("We have total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
@@ -15,7 +16,7 @@ func main() {
 	var lastName string
 	var eMail string
 	var userTickets uint
-	
+
 	// ask user for their name
 	fmt.Print("Enter your first name: ")
 	fmt.Scan(&firstName)
@@ -30,6 +31,12 @@ func main() {
 	fmt.Scan(&userTickets)
 
 	remainingTickets = remainingTickets - userTickets
+	bookings[0] = firstName + " " + lastName
+
+	fmt.Printf("The whole array: %v\n", bookings)
+	fmt.Printf("The first value: %v\n", bookings[0])
+	fmt.Printf("Array type: %T\n", bookings)
+	fmt.Printf("Array size: %v\n", len(bookings))
 
 	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation e-mail at %v\n", firstName, lastName, userTickets, eMail)
 	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
