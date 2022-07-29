0b54d78eb0cd51ea8f42d2bf050b608bc610f50a
p2well <p2well@outlook.com>
2022-07-29 13:41:55 +0200

    Clean up reminder comments

--- main.go
+++ main.go
@@ -6,22 +6,17 @@ func main() {
 	conferenceName := "p2well's Go Conference"
 	const conferenceTickets int = 50
 	var remainingTickets uint =  50
-
-	// fmt.Printf("conferenceTickets is %T, remainingTickets is %T, conferenceName is %T\n", conferenceName, remainingTickets, conferenceName)
-	
+		
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Printf("We have total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
-
+	
 	var firstName string
 	var lastName string
 	var eMail string
 	var userTickets uint
+	
 	// ask user for their name
-
-	// fmt.Println(remainingTickets)
-	// fmt.Println(&remainingTickets)
-
 	fmt.Print("Enter your first name: ")
 	fmt.Scan(&firstName)
 
