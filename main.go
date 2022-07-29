6a3649a5eafc4caa5c3f55002911d83072eaedd9
p2well <p2well@outlook.com>
2022-07-29 13:59:47 +0200

    Refactor bookings and clean-up

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ func main() {
 	conferenceName := "p2well's Go Conference"
 	const conferenceTickets int = 50
 	var remainingTickets uint =  50
-	var bookings [50]string
+	bookings := []string{}
 	
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Printf("We have total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
@@ -17,7 +17,6 @@ func main() {
 	var eMail string
 	var userTickets uint
 
-	// ask user for their name
 	fmt.Print("Enter your first name: ")
 	fmt.Scan(&firstName)
 
@@ -31,14 +30,10 @@ func main() {
 	fmt.Scan(&userTickets)
 
 	remainingTickets = remainingTickets - userTickets
-	bookings[0] = firstName + " " + lastName
-
-	fmt.Printf("The whole array: %v\n", bookings)
-	fmt.Printf("The first value: %v\n", bookings[0])
-	fmt.Printf("Array type: %T\n", bookings)
-	fmt.Printf("Array size: %v\n", len(bookings))
+	bookings = append(bookings, firstName + " " + lastName)
 
 	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation e-mail at %v\n", firstName, lastName, userTickets, eMail)
 	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
-	
+
+	fmt.Printf("These are all our bookings %v\n", bookings)
 }
